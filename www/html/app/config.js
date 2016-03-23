beccd1e5bed3965a44578f61d8d7a3f4d142511a
jsoon <serdeemail@gmail.com>
2016-03-23 22:19:38 +0800

    Completing profile and upload page

--- www/html/app/config.js
+++ www/html/app/config.js
@@ -2,11 +2,11 @@
  * requirejs 配置文件
  */
 define(function () {
-  return require.config({
-    baseUrl: './',
-    paths: {
-      magnific: 'lib/magnific-popup/1.0.0/jquery.magnific-popup',
-      dropzone: 'lib/dropzone/4.0.1/dropzone-amd-module'
-    }
-  });
+    return require.config({
+        baseUrl: './',
+        paths: {
+            magnific: 'lib/magnific-popup/1.1.0/jquery.magnific-popup',
+            dropzone: 'lib/dropzone/4.3.0/dropzone-amd-module'
+        }
+    });
 });
--- www/html/app/my/my-upload.js
+++ www/html/app/my/my-upload.js
@@ -6,19 +6,18 @@ define([
   'dropzone'
 ], function () {
 
-  // 照片上传
-  $('#JS_photo_upload').dropzone({
-    url: '/action',
-    method: 'post',
-    maxFilesize: 2, // MB
-    accept: function (file, done) {
-      if (file.name == "justinbieber.jpg") {
-        done("Naha, you don't.");
-      }
-      else {
-        done('upload');
-      }
-    }
-  });
+    // 照片上传
+    var dz = $('#J_PhotoUpload').dropzone({
+        url: '/action',
+        method: 'post',
+        maxFilesize: 2, // MB
+        accept: function (file, done) {
+            if (file.name == "justinbieber.jpg") {
+                done("Naha, you don't.");
+            } else {
+                done();
+            }
+        }
+    });
 
 });
